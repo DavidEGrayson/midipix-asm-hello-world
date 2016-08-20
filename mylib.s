d8878f1b5b3ecf60567c707ae1ceb27c045292dd
David Grayson <davidegrayson@gmail.com>
2016-08-20 10:53:55 -0700

    Various simplifications, finally removed the call to gcc.

--- mylib.s
+++ mylib.s
@@ -1,15 +1,12 @@
-	.file	"mylib.c"
-	.globl	shared_string
-
-	.section .got$shared_string,"r"
-	.global __imp_shared_string
+    .file "mylib.s"
+    .globl shared_string
+    .section .got$shared_string, "r"
+    .global __imp_shared_string
 __imp_shared_string:
-	.quad	shared_string
-	.linkonce discard
-
-	.data
-	.data
+    .quad shared_string
+    .linkonce discard
+    .data
 shared_string:
-	.ascii "woot\0"
-	.section .drectve
-	.ascii " -export:\"shared_string\",data"
+    .ascii "wootz\0"
+    .section .drectve
+    .ascii " -export:\"shared_string\",data"
